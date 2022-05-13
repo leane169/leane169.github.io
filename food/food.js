848e301d6fcaa16d9c0253405e5fae6a1e80605e
leane169 <91250393+leane169@users.noreply.github.com>
2022-05-13 11:59:09 +0800

    Add files via upload

---
+++ food/food.js
@@ -0,0 +1,29 @@
+function setup() {
+    initializeFields();
+    createCanvas(400, 300);
+    textSize(40);
+}
+
+var choice;
+
+function draw() {
+    background(color(0xE3, 0x87, 0x87));
+    fill(color(0xD6, 0x13, 0x13));
+    if (choice == 0)
+        rect(50, 50, 150, 2);
+    if (choice == 1)
+        rect(50, 100, 150, 2);
+    if (choice == 2)
+        rect(50, 150, 150, 2);
+    text("中餐吃泡麵", 50, 50);
+    text("中餐Uberea", 50, 100);
+    text("中餐foodpanda", 50, 150);
+}
+
+function mousePressed() {
+    choice = int(random(3));
+}
+
+function initializeFields() {
+    choice = -1;
+}
